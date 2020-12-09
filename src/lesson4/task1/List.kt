80a8a0f86af4eb5e0fb89dd553a27d63507597a2
Sevdat <sevdat1999@gmail.com>
2020-12-09 04:09:34 +0300

    improvements 3

--- src/lesson4/task1/List.kt
+++ src/lesson4/task1/List.kt
@@ -138,7 +138,7 @@ fun abs(v: List<Double>): Double {
  * Рассчитать среднее арифметическое элементов списка list. Вернуть 0.0, если список пуст
  */
 fun mean(list: List<Double>): Double =
-    if (list.isNotEmpty()) (list.sum() / list.size) else 0.0
+    if (list.isNotEmpty()) (list.sum() / list.size) else 0.00
 
 
 //    val i = list.sum()
