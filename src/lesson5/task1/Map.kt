c81336c1f130d56ee5a33aad62e757537da4fde9
Sevdat <sevdat1999@gmail.com>
2020-12-09 05:43:57 +0300

    improvements 3

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -153,7 +153,7 @@ fun subtractOf(a: MutableMap<String, String>, b: Map<String, String>) {
  * В выходном списке не должно быть повторяюихся элементов,
  * т. е. whoAreInBoth(listOf("Марат", "Семён, "Марат"), listOf("Марат", "Марат")) == listOf("Марат")
  */
-fun whoAreInBoth(a: List<String>, b: List<String>): List<String> = TODO()
+fun whoAreInBoth(a: List<String>, b: List<String>): List<String> = a.intersect(b.toSet()).toList()
 
 /**
  * Средняя (3 балла)
