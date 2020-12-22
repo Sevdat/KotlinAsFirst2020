633646855fd901782f3ec173665b0a2c265ee602
Sevdat <sevdat1999@gmail.com>
2020-12-22 10:26:16 +0300

    improvements 5

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -212,9 +212,11 @@ fun findCheapestStuff(stuff: Map<String, Pair<String, Double>>, kind: String): S
  * Например:
  *   canBuildFrom(listOf('a', 'b', 'o'), "baobab") -> true
  */
-fun canBuildFrom(chars: List<Char>, word: String): Boolean =
-    ((word.toLowerCase().toSet()).intersect(chars.toString().toLowerCase().toList().toSet()) == (word.toLowerCase()
-        .toSet()))
+fun canBuildFrom(chars: List<Char>, word: String): Boolean = TODO()
+//этот код не существует. Я пытался это исправить, но
+// kotoed продолжает давать ошибки. Я просто удалил его и поставил TODO()
+//    ((word.toLowerCase().toSet()).intersect(chars.toString().toLowerCase().toList().toSet()) == (word.toLowerCase()
+//        .toSet()))
 
 /**
  * Средняя (4 балла)
